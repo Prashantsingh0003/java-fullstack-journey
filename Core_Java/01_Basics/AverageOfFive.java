2b488de14a09176c1c17a595b1f3d7d55fa72a3c
Prashantsingh0003 <prashu9838@gmail.com>
2025-05-14 11:50:17 +0530

    Added basic Java programs

---
+++ Core_Java/01_Basics/AverageOfFive.java
@@ -0,0 +1,7 @@
+public class AverageOfFive {
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 30, d = 40, e = 50;
+        int avg = (a + b + c + d + e) / 5;
+        System.out.println("Average is: " + avg);
+    }
+}
